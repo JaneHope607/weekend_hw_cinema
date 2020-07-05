c7df2b472f0fe1e3b250ebdf50d9b437195e8325
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-05 10:39:14 +0100

    save method created for ticket class

--- weekend_hw_cinema/models/ticket.rb
+++ weekend_hw_cinema/models/ticket.rb
@@ -12,4 +12,14 @@ class Ticket
         @film_id = options['film_id'].to_i
     end
 
+    def save()
+        sql = "INSERT INTO tickets
+        (cutomer_id, film_id)
+        VALUES ($1, $2)
+        RETURNING id"
+        values = [@customer_id, @film_id]
+        result = SqlRunner.run(sql, values).first
+        @id = ticket['id'].to_i
+    end
+
 end
